e8e909cc6b9bc4d6b64be4e4a79268dbe4c3937f
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2023-07-14 13:09:34 +0300

    u
    u

--- app/src/main/java/com/itzik/notes/project/screens/DeletedNotes.kt
+++ app/src/main/java/com/itzik/notes/project/screens/DeletedNotes.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import com.itzik.notes.project.viewmodels.NoteViewModel
@@ -16,8 +19,21 @@ fun DeletedNotesScreen(
     navHostController: NavHostController,
     noteViewModel: NoteViewModel,
 ) {
-    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Deleted Notes")
+    ConstraintLayout(modifier = modifier.fillMaxSize()) {
+        val (titleText,deletedNotesList) =createRefs()
+        Text(
+            text = "Deleted Notes",
+            fontSize = 20.sp,
+            fontFamily = FontFamily.SansSerif,
+            color = Color.Blue,
+            modifier = modifier.constrainAs(titleText){
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+            }
+        )
+
+
     }
 
 }
--- app/src/main/java/com/itzik/notes/project/screens/NoteList.kt
+++ app/src/main/java/com/itzik/notes/project/screens/NoteList.kt
@@ -1,10 +1,12 @@
 package com.itzik.notes.project.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Outline
@@ -89,10 +92,10 @@ fun NoteListScreen(
                                     end.linkTo(addNote.start)
                                 }
                                 .padding(12.dp)
-
                                 .clickable {
                                     coroutineScope.launch {
                                         noteList = emptyList<Note>().toMutableList()
+                                        noteViewModel.addNoteToTrashBin(noteList)
                                     }
                                 },
                             contentDescription = "delete all",
@@ -128,7 +131,7 @@ fun NoteListScreen(
                 }
             )
         },
-
+        drawerBackgroundColor = colorResource(id = R.color.light_teal),
         drawerContent = {
             DrawerBody(
                 items = listOf(

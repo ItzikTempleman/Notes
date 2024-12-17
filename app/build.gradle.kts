c29cb6a3bb8e23d53442185b61100feb49c1f786
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-12-17 11:20:21 +0200

    r
    r

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.itzik.notes_"
         minSdk = 29
         targetSdk = 34
-        versionCode = 29
-        versionName = "29.0"
+        versionCode = 30
+        versionName = "30.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
